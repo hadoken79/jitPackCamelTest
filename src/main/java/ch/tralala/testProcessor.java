20b4ea45a8b93cd63d00dd70f672ab06f51b9c19
Linus Pauls <linus.pauls@irix.ch>
2021-08-29 14:26:03 +0200

    Revert "readded Test"
    This reverts commit db2eed2922b2b13e3109234271196604e58c2079.

--- src/main/java/ch/tralala/testProcessor.java
+++ src/main/java/ch/tralala/testProcessor.java
@@ -1,4 +1,3 @@
-// camel-k: dependency=github:hadoken79/jitPackCamelTest/v1.1
 package ch.tralala;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -7,7 +6,7 @@ public class testProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        StringProvider t = new StringProvider();
+        Test t = new Test();
         String a = exchange.getMessage().getBody().toString();
         exchange.getMessage().getHeaders().put("key2", StaticShit.addprop());
         exchange.getIn().setBody(t.getMsg(a));
