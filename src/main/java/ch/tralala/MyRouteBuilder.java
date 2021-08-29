12905a13fd7e7996eef4ec324c6fcb36f66e894d
Linus Pauls <linus.pauls@irix.ch>
2021-08-29 14:24:47 +0200

    Revert "remove main and route"
    This reverts commit 35895f45a9346e773a6c3fac05659d8ca850a4a4.

---
+++ src/main/java/ch/tralala/MyRouteBuilder.java
@@ -0,0 +1,22 @@
+package ch.tralala;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class MyRouteBuilder extends RouteBuilder {
+
+    /**
+     * Let's configure the Camel routing rules using Java code...
+     */
+    public void configure() {
+
+        getContext().setTracing(true);
+
+        from("timer://foo?fixedRate=true&period=60000")
+                .setProperty("key", constant("val"))
+                .setHeader("key", constant("val"))
+                .setBody(constant("foo"))
+                .process(new testProcessor());
+    }
+}
